430e00c481b8f4b2d42ddc051e0b881506abb529
danny-zero <sifuarrow@gmail.com>
2021-06-17 23:52:32 -0400

    wrap typed text

--- src/components/TypedReactHooksDemo.js
+++ src/components/TypedReactHooksDemo.js
@@ -3,21 +3,41 @@ import Typed from 'typed.js';
 import styles from './css/Typed.module.css';
 
 const TypedReactHooksDemo = () => {
+  const [show, setShow] = useState(false)
 	const el = useRef(null);
 	const typed = useRef(null);
 
   useEffect(() => {
+    // const options = {
+    // 	strings: [
+    //     "Hi!",
+    //     "I'm Danny, I'm a \n Software Engineer",
+    //     '<a href="#/projects" style="text-decoration: underline dodgerblue">Click here</a> \n to see some of my work'
+    //   ],
+    //   typeSpeed: 50,
+    //   backSpeed: 50,
+    // };
+    
     const options = {
     	strings: [
         "Hi!",
-        "I'm Danny, I'm a \n Software Engineer",
-        '<a href="#/projects" style="text-decoration: underline dodgerblue">Click here</a> \n to see some of my work'
+        "I'm Danny, I'm a Software Engineer",
+        '<a href="#/projects" style="text-decoration: underline dodgerblue">Click here</a> to see some of my work'
       ],
       typeSpeed: 50,
       backSpeed: 50,
     };
-    
+
+
     typed.current = new Typed(el.current, options);
+
+    const time = (options.strings.reduce((endGoal, curIter) => {
+      return endGoal + curIter.length 
+    }, 0) * 1.5) * 50
+
+    setTimeout(() => {
+      setShow(true)
+    }, time)
     
     return () => {
       typed.current.destroy();
@@ -26,8 +46,11 @@ const TypedReactHooksDemo = () => {
 
   return (
     <div className={styles.wrap}>
-      <div className={styles.typeWrap}>
-        <span style={{ whiteSpace: 'pre', fontSize:'25px' }} ref={el} />
+      <div>
+        <span className={styles.typeWrap} style={{ whiteSpace: 'pre-wrap', fontSize:'25px' }} ref={el} />
+        {/* {
+        show ? <h4 className="animate__animated animate__fadeIn">Hello</h4> : null
+      } */}
       </div>
     </div>
   );
