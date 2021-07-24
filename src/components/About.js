b443ea24c6eb4bb3e64345f421ebe0bc8d016d27
danny-zero <sifuarrow@gmail.com>
2021-07-23 22:18:34 -0400

    added sass icon and updated resume

--- src/components/About.js
+++ src/components/About.js
@@ -37,20 +37,21 @@ const About = () => {
                         <span style={{ whiteSpace: 'pre', fontSize:'25px' }} ref={el} />
                         <p>My name is Danny, and I'm a software engineer and photographer living in Miami, FL.</p>
                         <p>Before I became a software engineer I worked in the health industry for 10 years as a pharmacy technician in the in-patient setting of a local children's hospital. I would prepare both IV medication and oral solutions and work with different technologies in the dispensing and inventory management of pharmaceuticals such as Pyxis, DoseEdge, Cerner CPOE, etc. During this time in the healthcare field both in the hospital and attending EMT school&nbsp;I became comfortable in being detail-oriented in a high-stakes environment and working with many different types of professionals.</p>
-                        <p>I graduated Fullstack Academy's software engineering immersive and am experienced in JavaScript, React, Redux, HTML5, CSS3, Sequelize, PostgreSQL, Node, Express, React-Bootstrap, Bootstrap, AWS S3, and Jekyll</p>
+                        <p>I graduated Fullstack Academy's software engineering immersive and am experienced in JavaScript, React, Redux, HTML5, CSS3, Sass, Sequelize, PostgreSQL, Node, Express, React-Bootstrap, Bootstrap, AWS S3, and Jekyll</p>
                         <div className={styles.aboutIcons}>
                           <span className="iconify" data-icon="bx:bxl-javascript" data-inline="false"></span>
                           <span className="iconify" data-icon="bx:bxl-react" data-inline="false"></span>
                           <span className="iconify" data-icon="simple-icons:redux" data-inline="false"></span>
                           <span className="iconify" data-icon="icomoon-free:html-five" data-inline="false"></span>
-                          <span className="iconify" data-icon="logos:css-3-official" data-inline="false"></span>
+                          <span className="iconify" data-icon="bx:bxl-css3" data-inline="false"></span>
+                          <span className="iconify" data-icon="cib:sass-alt" data-inline="false"></span>
                           <span className="iconify" data-icon="file-icons:sequelize" data-inline="false"></span>
                           <span className="iconify" data-icon="simple-icons:postgresql" data-inline="false"></span>
                           <span className="iconify" data-icon="fa-brands:node" data-inline="false"></span>
                           <span className="iconify" data-icon="simple-icons:express" data-inline="false"></span>
                           <span className="iconify" data-icon="bi:bootstrap" data-inline="false"></span>
                           <span className="iconify" data-icon="simple-icons:amazons3" data-inline="false"></span>
-                          <span class="iconify" data-icon="cib:jekyll" data-inline="false"></span>
+                          <span className="iconify" data-icon="cib:jekyll" data-inline="false"></span>
                           {/* <span className="iconify" data-icon="cib:gatsby" data-inline="false"></span> */}
                         </div>
                         <br />
