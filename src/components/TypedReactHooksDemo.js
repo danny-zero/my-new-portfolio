401101b3a631f169864826cf7aafd71b8e261259
danny-zero <sifuarrow@gmail.com>
2022-03-02 12:15:39 -0500

    fixed typo in about and added arrow to home page

--- src/components/TypedReactHooksDemo.js
+++ src/components/TypedReactHooksDemo.js
@@ -5,7 +5,7 @@ import styles from './css/Typed.module.css';
 const TypedReactHooksDemo = () => {
   const [show, setShow] = useState(false)
 	const el = useRef(null);
-	const typed = useRef(null);
+	// const typed = useRef(null);
 
   useEffect(() => {
     const options = {
@@ -16,21 +16,28 @@ const TypedReactHooksDemo = () => {
       ],
       typeSpeed: 50,
       backSpeed: 50,
+      onComplete: function() {
+        setShow(true);
+      }
     };
 
+    const typed = new Typed(el.current, options)
 
-    typed.current = new Typed(el.current, options);
-    
-    return () => {
-      typed.current.destroy();
-    }
+    return () => typed.destroy();
   }, [])
+
+
 
   return (
     <div className={styles.wrap}>
       <div>
         <span className={styles.typeWrap} style={{ whiteSpace: 'pre-wrap', fontSize:'25px' }} ref={el} />
       </div>
+     <div className={styles.lowerContainer}>
+       {
+         show ? <a className="animate__animated animate__bounce" href="#/projects">&rarr;</a> : null
+       }
+     </div>
     </div>
   );
 }
